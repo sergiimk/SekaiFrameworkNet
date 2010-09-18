8177d87f007ef369fecf9994989b3bcfc5477394
Sergey Mikhtonyuk <mikhtonyuk@gmail.com>
2010-09-18 12:47:11 +0300

    Switched to LGPL

---
+++ src/framework/Core/Implementation/Misc/FreePool.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace framework.Core.Implementation
+{
+	class FreePool<T> where T: new()
+	{
+		LinkedList<T> m_items = new LinkedList<T>();
+
+		public T Allocate()
+		{
+			lock(m_items)
+			{
+				if(m_items.Count > 0)
+				{
+					T ret = m_items.Last.Value;
+					m_items.RemoveLast();
+					return ret;
+				}
+				return default(T);
+			}
+		}
+
+		public void Free(T item)
+		{
+			lock (m_items)
+			{
+				m_items.AddLast(item);
+			}
+		}
+	}
+}
